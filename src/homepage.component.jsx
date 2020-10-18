fdf864648433737cdeb044e4255ee106c4156d6d
Mariana Guzmán <mggzmn91@gmail.com>
2020-10-17 20:53:36 -0500

    created project with react app and added files form homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./homepage.styless.scss";
+const HomePage = () =>(
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">Men</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;

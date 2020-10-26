3036b9c3901facc382c25a1e999034639315b8dd
Mariana Guzmán <mggzmn91@gmail.com>
2020-10-26 17:25:55 -0600

    update collection-item

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./preview-collection.styles.scss";
-import CollectionItem from '../collection-item/collection-item.component'
+import CollectionItem from "../collection-item/collection-item.component";
 const PreviewCollection = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title}</h1>
     <div className="preview">
-      {items.filter((item,index)=>index<4).map(({id,...otherItemProps}) => (
-        <CollectionItem key={id} {...otherItemProps}/>
-      ))}
+      {items
+        .filter((item, index) => index < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
 );

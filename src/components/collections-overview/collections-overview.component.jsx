40c0abdd807733db87d92351eb0c7f4d68548744
Mariana Guzmán <mggzmn91@gmail.com>
2020-10-28 18:54:20 -0600

    Using  Reselect and Redux in other components

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selectors";
+
+
+import PreviewCollection from "../../components/preview-collection/preview-collection.component";
+import "./collections-overview.styles.scss";
+
+const CollectionsOverView = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <PreviewCollection key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(CollectionsOverView);
